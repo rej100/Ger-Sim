a1f87b473f1aea000377fc30e957a69a9ba5a998
rej100 <srej100@gmail.com>
2017-09-06 20:34:47 +0200

    Abandoning the project for now.

--- src/services/main.service.ts
+++ src/services/main.service.ts
@@ -20,8 +20,7 @@ class GeItem
   cashPassiveBonus:number;
   cashPassiveModifier:number;
 
-  ImmediateEffect:any;
-  CustomPassiveEffect:any;
+  customPassiveEffect:string;
 
   constructor()
   {
@@ -34,8 +33,7 @@ class GeItem
     this.wurstPassiveModifier = 0;
     this.cashPassiveBonus = 0;
     this.cashPassiveModifier = 0;
-    this.ImmediateEffect = function() {};
-    this.CustomPassiveEffect = function() {};
+    this.customPassiveEffect = "ud";
   }
 
   UpdateCost()
@@ -60,8 +58,24 @@ class GeItem
 //Item declarations:
 
 let iRod:GeItem = new GeItem();
+iRod.id = 0;
+iRod.name = "test";
 iRod.baseCost = 20;
-let iItems:GeItem[] = [iRod];
+iRod.baseCost = 4;
+iRod.wurstPassiveBonus = 0.2;
+iRod.customPassiveEffect = "eTest";
+
+let iTest:GeItem = new GeItem();
+iTest.id = 0;
+iTest.name = "testTwo";
+iTest.baseCost = 20;
+iTest.baseCost = 4;
+iTest.wurstPassiveBonus = 0.2;
+iTest.customPassiveEffect = "eTestTwo";
+
+let iItems:GeItem[];
+let itemNum:number = 2;
+iItems = [iRod, iTest];
 
 @Injectable()
 export class MainService
@@ -108,6 +122,7 @@ export class MainService
         this.LoadSave();
       }
       this.SaveThread();
+      this.ItemThread();
       this.IncomeThread();
     }
 
@@ -139,6 +154,23 @@ export class MainService
       this.cashWurstCost = Number(localStorage.getItem("cashWurstCost"));
     }
 
+    ItemThread = () =>
+    {
+      var helperWurstPower = 0;
+      var helperCashPower = 0;
+      for(var i = 0; i < itemNum; ++i)
+      {
+        console.log(iItems[i].lvl);
+        if(iItems[i].lvl > 0)
+        {
+          helperWurstPower += iItems[i].wurstPassiveBonus;
+          helperCashPower += iItems[i].cashPassiveBonus;
+        }
+      }
+      this.wurstPassivePower = helperWurstPower;
+      this.cashPassivePower = helperWurstPower;
+    }
+
     IncomeThread = () =>
     {
       //console.log(this.wurstAmount);
@@ -152,4 +184,5 @@ export class MainService
       this.Save();
       setTimeout(this.SaveThread, 2000);
     }
+
 }
